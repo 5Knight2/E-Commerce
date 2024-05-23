32e9313a79b81c638665dfc804fab5ac09356434
5Knight2 <pratiktarale258@gmail.com>
2024-05-23 18:11:22 +0530

    about page added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,12 +2,17 @@ import React from "react";
 import Header from './Header';
 import Footer from './Footer';
 import Products from "./Products";
+import About from '../About/About'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+const router=createBrowserRouter([{path:'/',element:<Products/>},
+{path:'/about',element:<About></About>}])
 
 const Home=(props)=>{
 
     return(<React.Fragment>
         <Header cartClicked={props.cartClicked}></Header>
-        <Products></Products>
+        <RouterProvider router={router}></RouterProvider>
           <Footer></Footer>
     </React.Fragment>
         
