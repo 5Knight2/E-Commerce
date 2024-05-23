dbfa9c97ff14c1fd46cd8747ba5226adbcb1569a
5Knight2 <pratiktarale258@gmail.com>
2024-05-23 13:12:54 +0530

    Cart count added

--- src/Components/Home/Header.js
+++ src/Components/Home/Header.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button, Navbar, Container, Nav } from 'react-bootstrap';
 import classes from './Header.module.css';
+import CartContext from '../../Store/CartContext';
 
 const Header = (props) => {
+
+  const ctx=useContext(CartContext)
+
   return (
     <React.Fragment>
     <Navbar bg="dark"  variant="dark" sticky='top'>
@@ -13,6 +17,7 @@ const Header = (props) => {
           <Nav.Link href="#about">About</Nav.Link>
         </Nav>
         <Button variant="secondary" onClick={props.cartClicked}>Cart</Button>
+        <p className={classes.p}>{ctx.items.length}</p>
       </Container>
     </Navbar>
     <Container fluid  className={classes.container}>
