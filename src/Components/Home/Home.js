542bda5001e92c9ca5df8ff701ce3527c970db51
5Knight2 <pratiktarale258@gmail.com>
2024-05-28 01:48:36 +0530

    fetch demo

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,16 +1,34 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Container } from "react-bootstrap";
 import classes from './Home.module.css'
 import TourTable from './TourTable'
 
 
 const Store = (props) => {
+
+const url='https://swapi.dev/api/films/'
+const [movies,setMovies]=useState([]);
+
+
     const data=[{date:'JUL16',
     place:'DETROIT, MI',
     name:'DTE ENERGY MUSIC THEATRE'},{date:'JUL26',
     place:'Toronto, ON',
     name:'BUDWEISER STAGE'}
 ];
+
+const getData=async()=>{
+
+    try{
+    let p=await fetch(url);
+    p=await p.json();
+    console.log(p)
+    setMovies(p.results[0].title)
+}
+catch(err){console.log(err)}
+
+}
+
     return (
       <React.Fragment>
         <Container className={classes.container} >
@@ -19,6 +37,8 @@ const Store = (props) => {
         
         </Container>
             <br></br>
+  <Button onClick={getData} >Get Data</Button>
+  <p>{movies}</p>
       </React.Fragment>
     );
   };
